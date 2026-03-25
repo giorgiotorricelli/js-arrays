846795a433210be74d2d74e06f763994543f83fb
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-25 15:33:47 +0100

    completato ultimo punto della cartella 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -52,4 +52,5 @@ console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString);

4545b372088cc002dd3a933b3ad57a6ed261015f
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-25 15:22:28 +0100

    concluso punto due della cartella due

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -28,7 +28,17 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  let currentName = teachers[i]; //dichiaro e assegno una variabile per dare più chiarezza
+  if (currentName.length >= 5) {
+    longNames.unshift(currentName);//utilizzo unshift invece di pop per rispettare l'ordine originale
+  }
+}
+
+console.log(longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
